e52b408583621d5b6f7540ba9545fd3601c732f8
thepsycongroo <znaifer9@gmail.com>
2026-06-05 13:45:56 -0600

    Proyecto escuela commit inicial

---
+++ src/test/java/com/alberto/escuela/EscuelaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.alberto.escuela;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EscuelaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
